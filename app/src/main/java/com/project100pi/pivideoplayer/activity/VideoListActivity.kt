34ce20b1fd5d1796155823890a5bf374fc660eb5
Sachin Verma <dev.sachinverma@hotmail.com>
2019-10-29 10:55:56 +0530

    V1: 1. Changed video list creating method calling from activity to view model. 2. Created a separate method for launching player.

--- app/src/main/java/com/project100pi/pivideoplayer/activity/VideoListActivity.kt
+++ app/src/main/java/com/project100pi/pivideoplayer/activity/VideoListActivity.kt
@@ -1,8 +1,13 @@
 package com.project100pi.pivideoplayer.activity
 
+import android.Manifest
+import android.content.Context
 import android.content.Intent
+import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
 import android.view.View
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
@@ -11,49 +16,70 @@ import androidx.recyclerview.widget.RecyclerView
 import com.project100pi.pivideoplayer.listeners.OnClickListener
 import com.project100pi.pivideoplayer.R
 import com.project100pi.pivideoplayer.utils.Constants
-import android.content.pm.PackageManager
 import androidx.core.content.ContextCompat
-import android.Manifest.permission.WRITE_EXTERNAL_STORAGE
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.view.ActionMode
 import androidx.appcompat.widget.Toolbar
 import androidx.core.app.ActivityCompat
 import butterknife.BindView
 import butterknife.ButterKnife
+import com.project100pi.library.misc.Util
 import com.project100pi.pivideoplayer.adapters.StorageFileAdapter
-import com.project100pi.pivideoplayer.utils.Constants.PERMISSION_REQUEST_CODE
 import com.project100pi.pivideoplayer.factory.MainViewModelFactory
 import com.project100pi.pivideoplayer.listeners.ItemDeleteListener
+import com.project100pi.pivideoplayer.model.FolderInfo
+import com.project100pi.pivideoplayer.utils.PermissionsUtil
 import kotlin.collections.ArrayList
 
 
-class MainActivity : AppCompatActivity(), OnClickListener, ItemDeleteListener {
+class VideoListActivity : AppCompatActivity(), OnClickListener, ItemDeleteListener, PermissionsUtil.ShowAlertCallback{
 
-    @BindView(R.id.rv_video_file_list) lateinit var  recyclerView: RecyclerView
+    @BindView(R.id.rv_video_file_list) lateinit var  rvVideoList: RecyclerView
     @BindView(R.id.folder_view_container) lateinit var mFolderViewContainer: View
     @BindView(R.id.folder_up_text) lateinit var folderUpText: TextView
     @BindView(R.id.folder_up_image) lateinit var folderUpIconImage: ImageView
     @BindView(R.id.anim_toolbar) lateinit var mToolbar: Toolbar
+    @BindView(R.id.tv_no_video_found_msg) lateinit var tvEmptyList: TextView
 
-    private lateinit var model: MainViewModel
+    private lateinit var model: VideoListViewModel
     private var adapter: StorageFileAdapter? = null
     private var actionModeCallback = ActionModeCallback()
     private var actionMode: ActionMode? = null
     private val mContext = this
+    private var granted: Boolean = false
+    val permission =  Manifest.permission.WRITE_EXTERNAL_STORAGE
+    private lateinit var permissionUtil: PermissionsUtil
+    private var videoListData: ArrayList<FolderInfo> = ArrayList()
+
     companion object {
         var mIsMultiSelectMode: Boolean = false
     }
 
-    private fun init() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        ButterKnife.bind(this)
         setSupportActionBar(mToolbar)
+
+        permissionUtil = PermissionsUtil(this, this)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if (!granted)
+            permissionUtil.checkorRequestPermission(permission)
+    }
+
+    private fun init() {
 
         val application = requireNotNull(this).application
         val viewModelFactory = MainViewModelFactory(this , application)
-        model = ViewModelProviders.of(this, viewModelFactory).get(MainViewModel::class.java)
+        model = ViewModelProviders.of(this, viewModelFactory).get(VideoListViewModel::class.java)
 
         val folderUpIcon = ContextCompat.getDrawable(this, R.drawable.folder_up)
         this.folderUpIconImage.setImageDrawable(folderUpIcon)
@@ -66,22 +92,11 @@ class MainActivity : AppCompatActivity(), OnClickListener, ItemDeleteListener {
             mFolderViewContainer.visibility = View.GONE
         }
 
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        ButterKnife.bind(this)
-
-        init()
-
-        if (!checkPermission()) {
-            requestPermission()
-        } else {
-            model.loadAllFolderData()
-        }
+        tvEmptyList.visibility = View.VISIBLE
+        rvVideoList.visibility = View.GONE
 
         observeForObservers()
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -98,26 +113,64 @@ class MainActivity : AppCompatActivity(), OnClickListener, ItemDeleteListener {
         }
     }
 
+    // Permissions
+
+    private fun requestPermission() {
+        ActivityCompat.requestPermissions(this,
+            arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.INTERNET),
+            Constants.PERMISSION_REQUEST_CODE)
+    }
+
+    private fun showBrightnessPermissionDialog(context: Context) {
+
+//        val builder = AlertDialog.Builder(context)
+//        builder.setCancelable(true)
+//        val alert = builder.create()
+//        builder.setMessage("Please give the permission to change brightness. \n Thanks ")
+//            .setCancelable(false)
+//            .setPositiveButton("OK") { dialog, id ->
+//                val intent = Intent(Settings.ACTION_MANAGE_WRITE_SETTINGS)
+//                intent.data = Uri.parse("package:" + context.packageName)
+//                context.startActivity(intent)
+//                alert.dismiss()
+//            }
+//        alert.show()
+
+        val intent = Intent(Settings.ACTION_MANAGE_WRITE_SETTINGS)
+        intent.data = Uri.parse("package:" + context.packageName)
+        context.startActivity(intent)
+    }
+
+    override fun showAlert() {
+        requestPermission()
+    }
+
+    override fun permissionGranted() {
+        init()
+    }
+
     override fun onRequestPermissionsResult(
         requestCode: Int,
         permissions: Array<out String>,
         grantResults: IntArray
     ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-
-        when(requestCode) {
-            PERMISSION_REQUEST_CODE -> {
-                if (grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                    model.loadAllFolderData()
-                }
+        when (requestCode) {
+            Constants.PERMISSION_REQUEST_CODE -> {
+                permissionUtil.checkorRequestPermission(permission = permissions[0])
             }
         }
     }
 
     private fun observeForObservers() {
         model.foldersListExposed.observe(this, Observer {
-            if (it != null)
+            if (it != null) {
+                videoListData = it
+                tvEmptyList.visibility = View.GONE
+                rvVideoList.visibility = View.VISIBLE
                 setAdapter()
+            }
         })
     }
 
@@ -128,26 +181,26 @@ class MainActivity : AppCompatActivity(), OnClickListener, ItemDeleteListener {
             adapter = StorageFileAdapter(this, R.layout.row_folder_item, this)
             val linearLayout = LinearLayoutManager(this)
             linearLayout.orientation = LinearLayoutManager.VERTICAL
-            recyclerView.layoutManager = linearLayout
-            recyclerView.adapter = adapter
+            rvVideoList.layoutManager = linearLayout
+            rvVideoList.adapter = adapter
 
         }
 
         if (model.MODE == Constants.FOLDER_VIEW) {
             setFoldersList()
         } else if (model.MODE == Constants.SONG_VIEW) {
-            folderUpText.text = model.foldersListExposed.value!![model.CURRENT_SONG_FOLDER_INDEX].path
+            folderUpText.text = videoListData[model.CURRENT_SONG_FOLDER_INDEX].path
             mFolderViewContainer.visibility = View.VISIBLE
             setSongsList()
         }
     }
 
     private fun setFoldersList() {
-        adapter?.submitList(model.foldersListExposed.value)
+        adapter?.submitList(videoListData)
     }
 
     private fun setSongsList() {
-        adapter?.submitList(model.foldersListExposed.value?.get(model.CURRENT_SONG_FOLDER_INDEX)?.songsList)
+        adapter?.submitList(videoListData[model.CURRENT_SONG_FOLDER_INDEX]?.songsList)
     }
 
     private fun toggleSelection(position: Int) {
@@ -185,25 +238,19 @@ class MainActivity : AppCompatActivity(), OnClickListener, ItemDeleteListener {
         if (!mIsMultiSelectMode) {
             if (model.MODE != Constants.SONG_VIEW) {
                 model.MODE = Constants.SONG_VIEW
-                folderUpText.text = model.foldersListExposed.value!![position].path
+                folderUpText.text = videoListData[position].path
                 mFolderViewContainer.visibility = View.VISIBLE
                 model.onItemClicked(position)
                 setAdapter()
             } else {
 
-                //Play the video
-                var currentVideo = model.foldersListExposed.value!![model.CURRENT_SONG_FOLDER_INDEX].songsList[position]
-
-                val playerIntent = Intent(this, Player::class.java)
-                playerIntent.putExtra(Constants.FILE_PATH, currentVideo.path)
-                val pathsList = ArrayList<String?>()
-                for ((tempPos, folder) in model.foldersListExposed.value!![model.CURRENT_SONG_FOLDER_INDEX].songsList.withIndex()) {
-                    if (tempPos >= position) {
-                        pathsList.add(folder.path)
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    if (Settings.System.canWrite(this)) {
+                        launcherPlayerActivity(position)
+                    } else {
+                        showBrightnessPermissionDialog(this)
                     }
                 }
-                playerIntent.putExtra(Constants.QUEUE, pathsList)
-                startActivity(playerIntent)
             }
         } else {
             toggleSelection(position)
@@ -211,16 +258,31 @@ class MainActivity : AppCompatActivity(), OnClickListener, ItemDeleteListener {
 
     }
 
+    private fun launcherPlayerActivity(position: Int) {
+        //Play the video
+        val currentVideo = videoListData[model.CURRENT_SONG_FOLDER_INDEX].songsList[position]
+
+        val playerIntent = Intent(this, PlayerActivity::class.java)
+        playerIntent.putExtra(Constants.FILE_PATH, currentVideo.path)
+        val pathsList = ArrayList<String?>()
+        for ((tempPos, folder) in videoListData[model.CURRENT_SONG_FOLDER_INDEX].songsList.withIndex()) {
+            if (tempPos >= position) {
+                pathsList.add(folder.path)
+            }
+        }
+        playerIntent.putExtra(Constants.QUEUE, pathsList)
+        startActivity(playerIntent)
+    }
 
     private fun doActionOnOverflowItemClick(position: Int, viewId: Int) {
-        val data = model.foldersListExposed.value!![model.CURRENT_SONG_FOLDER_INDEX].songsList[position]
+        val data = videoListData[model.CURRENT_SONG_FOLDER_INDEX].songsList[position]
 
         when (viewId) {
             R.id.itemPlay -> {
-                val playerIntent = Intent(this, Player::class.java)
+                val playerIntent = Intent(this, PlayerActivity::class.java)
                 playerIntent.putExtra(Constants.FILE_PATH, data.path)
                 val pathsList = ArrayList<String?>()
-                for ((tempPos, folder) in model.foldersListExposed.value!![model.CURRENT_SONG_FOLDER_INDEX].songsList.withIndex()) {
+                for ((tempPos, folder) in videoListData[model.CURRENT_SONG_FOLDER_INDEX].songsList.withIndex()) {
                     if (tempPos >= position) {
                         pathsList.add(folder.path)
                     }
@@ -233,19 +295,6 @@ class MainActivity : AppCompatActivity(), OnClickListener, ItemDeleteListener {
                 model.delete(listOf(position), false, this)
             }
         }
-    }
-
-    private fun checkPermission(): Boolean {
-        val result = ContextCompat.checkSelfPermission(applicationContext, WRITE_EXTERNAL_STORAGE)
-        return result == PackageManager.PERMISSION_GRANTED
-    }
-
-    private fun requestPermission() {
-        ActivityCompat.requestPermissions(
-            this,
-            arrayOf(WRITE_EXTERNAL_STORAGE),
-            PERMISSION_REQUEST_CODE
-        )
     }
 
     override fun onBackPressed() {
