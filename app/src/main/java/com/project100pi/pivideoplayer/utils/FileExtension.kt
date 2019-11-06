9d601504de5fb7159d6811a1609a3293011b52f2
Sachin Verma <dev.sachinverma@hotmail.com>
2019-11-06 16:10:07 +0530

    V1: Added more file extension supported by exo player.

--- app/src/main/java/com/project100pi/pivideoplayer/utils/FileExtension.kt
+++ app/src/main/java/com/project100pi/pivideoplayer/utils/FileExtension.kt
@@ -3,7 +3,7 @@ package com.project100pi.pivideoplayer.utils
 object FileExtension  {
 
     private val exts = listOf("mp4", "m4a", "m4v", "f4v", "f4a",
-        "m4b", "m4r", "m4b", "mov")
+        "m4b", "m4r", "m4b", "mov", "mkv", "fly", "webm", "wmv", "avi")
 
     fun isVideo(path: String): Boolean {
         var ext = path.substring(path.lastIndexOf(".") + 1)
