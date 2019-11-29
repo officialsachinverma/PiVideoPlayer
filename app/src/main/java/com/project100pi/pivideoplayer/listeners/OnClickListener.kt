2412e5d2b70e18cbbf8c1bce243f4ce59441af0e
Sachin Verma <dev.sachinverma@hotmail.com>
2019-11-29 16:26:56 +0530

    Type : MISC Title : NA ID : NA Description : Added Comments What was changed : changed method name from onDirectorySelected -> onItemSelected Why was it changed : to maintain consistency in method names across the interface

--- app/src/main/java/com/project100pi/pivideoplayer/listeners/OnClickListener.kt
+++ app/src/main/java/com/project100pi/pivideoplayer/listeners/OnClickListener.kt
@@ -1,7 +1,41 @@
 package com.project100pi.pivideoplayer.listeners
 
+/**
+ * This interface provides three methods
+ * The caller of this interface methods
+ * should implement [View.OnClickListener]
+ * and [View.OnLongClickListener]
+ * the implementer of this interface will get the
+ * LongClicked and ClickEvent
+ *
+ * @since v1
+ *
+ * [onItemLongClicked]
+ * [onItemSelected]
+ * [onOverflowItemClick]
+ */
 interface OnClickListener {
+
+    /**
+     * Called when a user clicked on an item for a long time.
+     *
+     * @param position Int the position of the item which is clicked.
+     * @return Boolean
+     */
     fun onItemLongClicked(position: Int): Boolean
-    fun onDirectorySelected(position: Int)
+
+    /**
+     * Called when a user clicked on an item.
+     *
+     * @param position Int
+     */
+    fun onItemSelected(position: Int)
+
+    /**
+     * Called when overflow menu of an item is clicked
+     *
+     * @param position Int
+     * @param viewId Int
+     */
     fun onOverflowItemClick(position: Int, viewId: Int)
 }
