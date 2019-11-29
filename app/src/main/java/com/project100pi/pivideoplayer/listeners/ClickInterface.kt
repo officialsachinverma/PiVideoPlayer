2cab039532b1e186ec767159c29587109d5562cc
Sachin Verma <dev.sachinverma@hotmail.com>
2019-11-29 16:30:11 +0530

    Type : MISC Title : NA ID : NA Description : Duplicate interface What was changed : Removed ClickInterface Why was it changed : Not required

--- app/src/main/java/com/project100pi/pivideoplayer/listeners/ClickInterface.kt
+++
@@ -1,7 +0,0 @@
-package com.project100pi.pivideoplayer.listeners
-
-interface ClickInterface {
-    fun onItemClicked(position: Int)
-    fun onBackFolderPressed()
-    fun onItemLongClicked(position: Int): Boolean
-}
