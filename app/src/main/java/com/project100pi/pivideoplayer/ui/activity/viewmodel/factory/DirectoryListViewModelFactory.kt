6f83a47029f712190b9771dc808a119fd09d5a14
Sachin Verma <dev.sachinverma@hotmail.com>
2019-11-29 16:39:47 +0530

    Type : MISC Title : NA ID : NA Description : Removed application and added ItemDeleteListener in constructor What was changed : Removed application and added ItemDeleteListener in constructor Why was it changed : 1. Application was not required as we can take application context from context. 2. Made ItemDeleteListener a global variable

--- app/src/main/java/com/project100pi/pivideoplayer/ui/activity/viewmodel/factory/DirectoryListViewModelFactory.kt
+++ app/src/main/java/com/project100pi/pivideoplayer/ui/activity/viewmodel/factory/DirectoryListViewModelFactory.kt
@@ -1,20 +1,20 @@
 package com.project100pi.pivideoplayer.ui.activity.viewmodel.factory
 
-import android.app.Application
 import android.content.Context
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.project100pi.pivideoplayer.listeners.ItemDeleteListener
 import com.project100pi.pivideoplayer.ui.activity.viewmodel.DirectoryListViewModel
 
 class DirectoryListViewModelFactory(private val context: Context,
-                                    private val application: Application
+                                    private val itemDeleteListener: ItemDeleteListener
 ) : ViewModelProvider.Factory {
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+
         if (modelClass.isAssignableFrom(DirectoryListViewModel::class.java)) {
-            return DirectoryListViewModel(
-                context,
-                application
-            ) as T
+
+            return DirectoryListViewModel(context, itemDeleteListener) as T
+
         }
         throw IllegalArgumentException("Unknown ViewModel class")
     }
