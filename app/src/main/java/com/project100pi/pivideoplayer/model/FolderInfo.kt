331c9d705c2bf77f47d4e761f23eba8b74e02de7
Sachin Verma <dev.sachinverma@hotmail.com>
2019-11-29 16:36:33 +0530

    Type : MISC Title : NA ID : NA Description : Added Comments and changed variable names What was changed : 1. filesList -> videoInfoList 2. fileId -> videoId 3. fileName -> videoName 4. filePath -> videoPath 5. fileDuration -> durationInMs Why was it changed :

--- app/src/main/java/com/project100pi/pivideoplayer/model/FolderInfo.kt
+++ app/src/main/java/com/project100pi/pivideoplayer/model/FolderInfo.kt
@@ -1,12 +1,33 @@
 package com.project100pi.pivideoplayer.model
 
+/**
+ * Created by Sachin Verma on 2019-11-20.
+ *
+ * This class holds information about a folder
+ * which contains videos. the information of those videos are getting
+ * stored in videoFilesInfoList
+ *
+ * @since v1
+ */
+
 data class FolderInfo(var folderName: String = "",
                       var folderPath: String = ""){
 
-    val filesList: ArrayList<VideoTrackInfo> = ArrayList()
+    /**
+     * This list holds information about videos under this folder
+     */
+    val videoInfoList: ArrayList<VideoTrackInfo> = ArrayList()
 
-    fun addFile(fileId: Int, fileName: String, filePath: String, fileDuration: Long) {
-        filesList.add(VideoTrackInfo(fileId, fileName, filePath, fileDuration))
+    /**
+     * Adds a new information to the videoFilesInfoList
+     *
+     * @param videoId Int Stores value of _id in media database
+     * @param videoName String Stores video name
+     * @param videoPath String Stores video path
+     * @param durationInMs Long Stores video duration in millis
+     */
+    fun addVideoInfoToList(videoId: Int, videoName: String, videoPath: String, durationInMs: Long) {
+        videoInfoList.add(VideoTrackInfo(videoId, videoName, videoPath, durationInMs))
 
     }
 
