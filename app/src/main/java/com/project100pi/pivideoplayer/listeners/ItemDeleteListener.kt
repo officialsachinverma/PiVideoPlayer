cc8ff33590812f22c003b67eaf2974141f9a8163
Sachin Verma <dev.sachinverma@hotmail.com>
2019-11-29 16:27:59 +0530

    Type : MISC Title : NA ID : NA Description : Added Comments What was changed : Why was it changed :

--- app/src/main/java/com/project100pi/pivideoplayer/listeners/ItemDeleteListener.kt
+++ app/src/main/java/com/project100pi/pivideoplayer/listeners/ItemDeleteListener.kt
@@ -1,9 +1,37 @@
 package com.project100pi.pivideoplayer.listeners
 
-import java.io.File
+/**
+ * This Interface provides three methods
+ * The implementer of this interface has
+ * precise control over the status of delete
+ * operation, any error occurred while deletion operation
+ * and if permission is required to delete a file from
+ * sd card.
+ *
+ * @since v1
+ *
+ * [showPermissionForSdCard]
+ * [onDeleteSuccess]
+ * [onDeleteError]
+ */
 
 interface ItemDeleteListener {
+
+    /**
+     * Called when permission for sd card is required.
+     */
     fun showPermissionForSdCard()
+
+    /**
+     * Called when delete operation is successful.
+     *
+     * @param listOfIndexes List<Int> List of indices of videos
+     * which are deleted.
+     */
     fun onDeleteSuccess(listOfIndexes: List<Int>)
+
+    /**
+     * Called when an error occurs while executing the delete operation.
+     */
     fun onDeleteError()
 }
