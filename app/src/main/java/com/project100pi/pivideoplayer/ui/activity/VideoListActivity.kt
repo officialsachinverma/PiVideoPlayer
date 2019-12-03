1aa486541c54a25d6abf8db43e96eeec9411ed26
Sachin Verma <dev.sachinverma@hotmail.com>
2019-12-03 11:21:25 +0530

    Type : MISC Title : NA ID : NA Description : Added Spaces, comments, using .let for intent, added methods for setting up toolbar and homeUpAsButton, removed hardcoded strings, removed onBackPressed, added clearAdapterSelection method, added methods to hide and show ui components What was changed : 1. Using .let while getting data from intent 2. Added methods for setting up toolbar and homeUpAsButton 3. Removed hardcoded strings 4. Removed onBackPressed 5. Added clearAdapterSelection method 6. Added methods to hide and show ui components Why was it changed : 1. Intent is a nullable property and can be changed to null (eg: by other threads) before I fetch any data. .let will allow me to save the non null reference inside the body of let and use it. 2. To improve readability 3. To maintain consistency 4. By default pressing on back button on toolbar finishes the activity no need to implement this method 5. To simplify clear adapter selection 6. To simplify ui hide and show operation

--- app/src/main/java/com/project100pi/pivideoplayer/ui/activity/VideoListActivity.kt
+++ app/src/main/java/com/project100pi/pivideoplayer/ui/activity/VideoListActivity.kt
@@ -3,7 +3,6 @@ package com.project100pi.pivideoplayer.ui.activity
 import android.annotation.TargetApi
 import android.content.Context
 import android.content.Intent
-import android.content.SharedPreferences
 import android.os.Build
 import android.os.Bundle
 import android.view.Menu
@@ -34,20 +33,21 @@ import com.project100pi.pivideoplayer.ui.activity.viewmodel.VideoListViewModel
 import com.project100pi.pivideoplayer.ui.activity.viewmodel.factory.VideoListViewModelFactory
 import com.project100pi.pivideoplayer.ui.adapters.VideoFilesAdapter
 import com.project100pi.pivideoplayer.utils.Constants
-import com.project100pi.pivideoplayer.utils.Constants.SHARED_PREFERENCES
 import com.project100pi.pivideoplayer.utils.ContextMenuUtil
 import java.io.File
-import java.lang.NullPointerException
 
 
 class VideoListActivity : AppCompatActivity(), OnClickListener, ItemDeleteListener {
 
     @BindView(R.id.rv_video_file_list)
     lateinit var  rvVideoList: RecyclerView
+
     @BindView(R.id.video_list_toolbar)
     lateinit var mToolbar: Toolbar
+
     @BindView(R.id.tv_no_video_found_msg)
     lateinit var tvEmptyList: TextView
+
     @BindView(R.id.pg_video_waiting)
     lateinit var pgWaiting: ProgressBar
 
@@ -63,8 +63,17 @@ class VideoListActivity : AppCompatActivity(), OnClickListener, ItemDeleteListen
     private var actionMode: ActionMode? = null
 
     companion object {
+
         var mIsMultiSelectMode: Boolean = false
 
+        // starter pattern is more strict approach to starting an activity.
+        // Main purpose is to improve more readability, while at the same time
+        // decrease code complexity, maintenance costs, and coupling of your components.
+
+        // Read more: https://blog.mindorks.com/learn-to-write-good-code-in-android-starter-pattern
+        // https://www.programming-books.io/essential/android/starter-pattern-d2db17d348ca46ce8979c8af6504f018
+
+        // Using starter pattern to start this activity
         fun start(context: Context, directoryName: String, directoryPath: String) {
             val intent = Intent(context, VideoListActivity::class.java)
             intent.putExtra(Constants.Storage.DIRECTORY_NAME, directoryName)
@@ -79,26 +88,43 @@ class VideoListActivity : AppCompatActivity(), OnClickListener, ItemDeleteListen
         ButterKnife.bind(this)
         setSupportActionBar(mToolbar)
         this.intent?.let {
-            //videoListData = intent.getParcelableArrayListExtra("videoList") ?: arrayListOf()
-            directoryName = intent.getStringExtra(Constants.Storage.DIRECTORY_NAME) ?: ""
-            directoryPath = intent.getStringExtra(Constants.Storage.DIRECTORY_PATH) ?: ""
+            directoryName = it.getStringExtra(Constants.Storage.DIRECTORY_NAME) ?: ""
+            directoryPath = it.getStringExtra(Constants.Storage.DIRECTORY_PATH) ?: ""
             init()
         }
     }
 
+    /**
+     * Initialises stuffs and performs some initial things
+     */
+
     private fun init() {
 
-        val viewModelFactory = VideoListViewModelFactory(this, directoryPath, application)
+        val viewModelFactory = VideoListViewModelFactory(this, directoryPath, directoryName, this)
         videoListViewModel = ViewModelProviders.of(this, viewModelFactory).get(VideoListViewModel::class.java)
+
+        setTitleOnToolbar()
+        setHomeUpAsButton()
 
         initAdapter()
 
         observeForObservable()
     }
 
+    /**
+     * This method contains all other methods
+     * who are observing a particular thing
+     */
+
     private fun observeForObservable() {
         observeForVideoList()
     }
+
+    /**
+     * This method observes Search Result List
+     * whenever data is available we have to set
+     * that data on the adapter
+     */
 
     private fun observeForVideoList(){
         videoListViewModel.filesList.observe(this, Observer {
@@ -107,22 +133,48 @@ class VideoListActivity : AppCompatActivity(), OnClickListener, ItemDeleteListen
         })
     }
 
-    private fun setDataToAdapter(){
+    /**
+     * This method submits list to the adapter
+     * and hides/shows empty msg and waiting sings
+     * based on data availability
+     */
 
+    private fun setDataToAdapter(){
         if (videoListData.size > 0) {
             hideEmptyListMsg()
             showVideoList()
             hideWaitingSign()
+            adapter.submitList(videoListData)
         } else {
             showEmptyListMsg()
             hideVideoList()
             hideWaitingSign()
         }
-
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        supportActionBar?.title = directoryName
-        adapter.submitList(videoListData)
     }
+
+    /**
+     * sets toolbar title which is the folder name which
+     * user has selected
+     */
+
+    private fun setTitleOnToolbar() {
+        supportActionBar?.title = directoryName
+    }
+
+    /**
+     * enables home back button
+     */
+
+    private fun setHomeUpAsButton() {
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    /**
+     * this method initialises the adapter
+     * WARNING: Adapter is a late init property
+     * it has to be initialised before using anywhere
+     * kotlin won't even allow you to put null check on it
+     */
 
     private fun initAdapter() {
         adapter = VideoFilesAdapter(this, R.layout.row_video_item, this)
@@ -153,12 +205,15 @@ class VideoListActivity : AppCompatActivity(), OnClickListener, ItemDeleteListen
     @TargetApi(Build.VERSION_CODES.KITKAT)
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
+        hideWaitingSign()
         when (requestCode) {
-            100 -> {
+            Constants.Permission.SD_CARD_WRITE_PERMISSION_REQUEST_CODE -> {
                 if (resultCode == RESULT_OK && data != null) {
+                    // when user will select the external sd card we will receive it's uri in data
                     val sdCardUri = data.data
-                    TinyDB.putString(Constants.SD_CARD_URI, sdCardUri.toString())
-                    // Persist access permissions.
+                    // saving the sd card uri in tiny db (shared preferences)
+                    TinyDB.putString(Constants.ExternalSDCard.SD_CARD_URI, sdCardUri.toString())
+                    // Persist access permissions
                     val takeFlags = data.flags and (Intent.FLAG_GRANT_READ_URI_PERMISSION or Intent.FLAG_GRANT_WRITE_URI_PERMISSION)
                     contentResolver.takePersistableUriPermission(sdCardUri!!, takeFlags)
 
@@ -168,22 +223,45 @@ class VideoListActivity : AppCompatActivity(), OnClickListener, ItemDeleteListen
         }
     }
 
+    /**
+     * starts search activity
+     */
+
     private fun launchSearchActivity() {
         SearchActivity.start(this)
     }
+
+    /**
+     * calls playVideo method which will generate data
+     * and pass it to player activity
+     * it takes the position of selected video
+     *
+     * @param position Int
+     */
 
     private fun launchPlayerActivity(position: Int) {
         playVideo(position, false)
     }
 
-    override fun onBackPressed() {
-        super.onBackPressed()
-        finish()
-    }
+    /**
+     * This method calls playVideo which will generate
+     * data and launches player activity
+     */
 
     private fun playSelectedVideos() {
         playVideo(-1, true)
     }
+
+    /**
+     * This method generates data and starts player activity
+     * it takes the position of selected video in case user
+     * has selected a single video
+     * it also takes a param isMultiple which shows whether the
+     * selection is of multiple videos or not
+     *
+     * @param position Int
+     * @param isMultiple Boolean
+     */
 
     private fun playVideo(position: Int, isMultiple: Boolean) {
         try {
@@ -191,7 +269,7 @@ class VideoListActivity : AppCompatActivity(), OnClickListener, ItemDeleteListen
             val metaDataList = arrayListOf<VideoMetaData>()
             if (!isMultiple) {
 //                val metadata = directoryListViewModel.getVideoMetaData(currentVideo.folderId)
-//           playerIntent.putExtra(Constants.FILE_PATH, currentVideo.path)
+//                playerIntent.putExtra(Constants.FILE_PATH, currentVideo.path)
                 //playerIntent.putExtra(Constants.FILE_PATH, metadata)
 
                 for (folder in videoListData) {
@@ -200,7 +278,9 @@ class VideoListActivity : AppCompatActivity(), OnClickListener, ItemDeleteListen
                 PlayerActivity.start(this, metaDataList, position)
             } else {
                 for(selectedItemPosition in adapter.getSelectedItems()) {
-//                    metaDataList.add(directoryListViewModel.getVideoMetaData(videoListData[directoryListViewModel.currentSongFolderIndex].songsList[selectedItemPosition].folderId)!!)
+//                    metaDataList.add(directoryListViewModel
+//                    .getVideoMetaData(videoListData[directoryListViewModel
+//                    .currentSongFolderIndex].songsList[selectedItemPosition].folderId)!!)
                     metaDataList.add(
                         VideoMetaData(
                             videoListData[selectedItemPosition]._Id,
@@ -217,6 +297,17 @@ class VideoListActivity : AppCompatActivity(), OnClickListener, ItemDeleteListen
         }
     }
 
+    /**
+     * This callback is called when overflow menu item is clicked
+     * it takes the position of selected video and the Id of view (menu item)
+     * on which user has clicked to perform that specific action only
+     * NOTE: In case of folder we do not have overflow menus
+     * as of now and hence, left empty
+     *
+     * @param position Int
+     * @param viewId Int
+     */
+
     private fun doActionOnOverflowItemClick(position: Int, viewId: Int) {
 
         when (viewId) {
@@ -231,6 +322,15 @@ class VideoListActivity : AppCompatActivity(), OnClickListener, ItemDeleteListen
             }
         }
     }
+
+    /**
+     * this method creates an intent chooser
+     * to display all the apps through which
+     * user can share the videos
+     * it takes position of video which is selected by user
+     *
+     * @param position Int
+     */
 
     private fun shareVideos(position: Int) {
         val currentVideo = videoListData[position]
@@ -264,7 +364,7 @@ class VideoListActivity : AppCompatActivity(), OnClickListener, ItemDeleteListen
                     videoListViewModel.shareMultipleVideos(adapter.getSelectedItems())
                 }
                 R.id.multiChoiceDelete -> {
-                    showMultiDeleteConfirmation()
+                    showMultiDeleteConfirmation(adapter.getSelectedItems())
                 }
             }
             // We have to end the multi select, if the user clicks on an option other than select all
@@ -277,10 +377,25 @@ class VideoListActivity : AppCompatActivity(), OnClickListener, ItemDeleteListen
             mIsMultiSelectMode = false
             actionMode = null
             showToolbar()
-            adapter.clearSelection()
+            clearAdapterSelection()
         }
 
     }
+
+    /**
+     * Clears the selected items in adapter
+     */
+    private fun clearAdapterSelection() {
+        adapter.clearSelection()
+    }
+
+    /**
+     * this method handles selection of an item in case of
+     * multi selection. It takes the position of video which
+     * is clicked or selected
+     *
+     * @param position Int
+     */
 
     private fun toggleSelection(position: Int) {
         adapter.toggleSelection(position)
@@ -297,6 +412,15 @@ class VideoListActivity : AppCompatActivity(), OnClickListener, ItemDeleteListen
         }
     }
 
+    /**
+     * This method triggers action mode when user long presses
+     * on a video
+     * it takes the position of video which user has selected
+     *
+     * @param position Int
+     * @return Boolean
+     */
+
     override fun onItemLongClicked(position: Int): Boolean {
         if (actionMode == null) {
             actionMode = startSupportActionMode(actionModeCallback)
@@ -307,7 +431,17 @@ class VideoListActivity : AppCompatActivity(), OnClickListener, ItemDeleteListen
         return true
     }
 
-    override fun onDirectorySelected(position: Int) {
+    /**
+     * This method gets called when a user clicks/selects a
+     * video.
+     * If multi select mode is not active then, navigate user to player activity
+     * where it will start playing the video, otherwise toggleSelection
+     * it takes the position of video which user has selected
+     *
+     * @param position Int
+     */
+
+    override fun onItemSelected(position: Int) {
         if (!mIsMultiSelectMode) {
             launchPlayerActivity(position)
         } else {
@@ -315,22 +449,50 @@ class VideoListActivity : AppCompatActivity(), OnClickListener, ItemDeleteListen
         }
     }
 
+    /**
+     * This callback is called when overflow menu item is clicked
+     * it takes the position of selected video and the Id of view (menu item)
+     * on which user has clicked to perform that specific action only
+     * NOTE: In case of folder we do not have overflow menus
+     * as of now and hence, left empty
+     *
+     * @param position Int
+     * @param viewId Int
+     */
+
     override fun onOverflowItemClick(position: Int, viewId: Int) {
         if (!mIsMultiSelectMode) {
             doActionOnOverflowItemClick(position, viewId)
         }
     }
 
+    /**
+     * This method is called when application does not
+     * have permission to read or write in external sd card
+     * It opens up a system activity where user can navigate
+     * to external sd card and click on select button on which
+     * it will callback to onActivity result with path to external sd card
+     */
+
     override fun showPermissionForSdCard() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             val intent = Intent(Intent.ACTION_OPEN_DOCUMENT_TREE)
-            startActivityForResult(intent, 100)
+            startActivityForResult(intent, Constants.Permission.SD_CARD_WRITE_PERMISSION_REQUEST_CODE)
         } else {
             Toast.makeText(this, R.string.do_operation_again, Toast.LENGTH_SHORT).show()
         }
     }
 
+    /**
+     * This method gets called when delete operation is successful
+     * it gives list of indices of item which are hard deleted and now
+     * supposed to be update on UI
+     *
+     * @param listOfIndexes List<Int>
+     */
+
     override fun onDeleteSuccess(listOfIndexes: List<Int>) {
+        hideWaitingSign()
         for(position in listOfIndexes) {
             videoListViewModel.removeElementAt(position)
             adapter.notifyItemRemoved(position)
@@ -343,61 +505,118 @@ class VideoListActivity : AppCompatActivity(), OnClickListener, ItemDeleteListen
         ).show()
     }
 
+    /**
+     * This method is called when an error occurred while
+     * executing the delete operation
+     */
+
     override fun onDeleteError() {
+        hideWaitingSign()
         Toast.makeText(this, R.string.error_occurred_while_deleting_videos, Toast.LENGTH_SHORT).show()
     }
 
-    private fun showMultiDeleteConfirmation() {
+    /**
+     * This method shows a confirmation dialog for deletion of multiple videos
+     * it records two user responses
+     * YES -> Execute delete operation
+     * NO -> Cancels operation execution
+     */
+
+    private fun showMultiDeleteConfirmation(listOfIndices: List<Int>) {
         AlertDialog.Builder(this)
             .setTitle(R.string.delete)
             .setMessage("Are you sure you want to delete this ${adapter.getSelectedItemCount()} video(s)?")
             .setPositiveButton(android.R.string.yes) { _, _ ->
-                videoListViewModel.deleteVideo(adapter.getSelectedItems(), this)
+                showWaitingSign()
+                videoListViewModel.deleteVideo(listOfIndices)
             }
             .setNegativeButton(android.R.string.no, null)
             .setCancelable(false)
             .show()
     }
+
+    /**
+     * This method shows a confirmation dialog for deletion of single video
+     * it takes the position of video which user has selected
+     * Dialog records two user responses
+     * YES -> Execute delete operation
+     * NO -> Cancel the operation
+     *
+     * @param position Int
+     */
 
     private fun showDeleteConfirmation(position: Int) {
         AlertDialog.Builder(this)
             .setTitle(R.string.delete)
             .setMessage(R.string.delete_confirmation_msg)
             .setPositiveButton(android.R.string.yes) { _, _ ->
-                videoListViewModel.deleteVideo(listOf(position), this)
+                showWaitingSign()
+                videoListViewModel.deleteVideo(listOf(position))
             }
             .setNegativeButton(android.R.string.no, null)
             .setCancelable(false)
             .show()
     }
 
+    /**
+     * hides toolbar
+     */
+
     private fun hideToolbar() {
         mToolbar.visibility = View.GONE
     }
+
+    /**
+     * shows toolbar
+     */
 
     private fun showToolbar() {
         mToolbar.visibility = View.VISIBLE
     }
 
+    /**
+     * hides empty list msg
+     */
+
     private fun hideEmptyListMsg() {
         tvEmptyList.visibility = View.GONE
     }
+
+    /**
+     * shows empty list msg
+     */
 
     private fun showEmptyListMsg() {
         tvEmptyList.visibility = View.VISIBLE
     }
 
+    /**
+     * hides video list
+     */
+
     private fun hideVideoList() {
         rvVideoList.visibility = View.GONE
     }
+
+    /**
+     * shows video list
+     */
 
     private fun showVideoList() {
         rvVideoList.visibility = View.VISIBLE
     }
 
+    /**
+     * hides waiting sign
+     */
+
     private fun hideWaitingSign() {
         pgWaiting.visibility = View.GONE
     }
+
+    /**
+     * shows waiting sign
+     */
 
     private fun showWaitingSign() {
         pgWaiting.visibility = View.VISIBLE
