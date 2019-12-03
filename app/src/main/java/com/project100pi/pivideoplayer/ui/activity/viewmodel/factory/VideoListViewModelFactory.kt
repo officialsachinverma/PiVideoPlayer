2f981e9fe48363e8d71baab31ab8eb43b5369080
Sachin Verma <dev.sachinverma@hotmail.com>
2019-12-03 10:42:28 +0530

    Type : MISC Title : NA ID : NA Description : Removed application, added folderName and ItemDeleteListener as param What was changed : Primary constructor of VideoListViewModelFactory Why was it changed : 1. Removed application from primary constructor because now class extends ViewModel rather than AndroidViewModel. And not require any more 2. Added folderName and ItemDeleteListener to simplify delete operation.

--- app/src/main/java/com/project100pi/pivideoplayer/ui/activity/viewmodel/factory/VideoListViewModelFactory.kt
+++ app/src/main/java/com/project100pi/pivideoplayer/ui/activity/viewmodel/factory/VideoListViewModelFactory.kt
@@ -1,22 +1,22 @@
 package com.project100pi.pivideoplayer.ui.activity.viewmodel.factory
 
-import android.app.Application
 import android.content.Context
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.project100pi.pivideoplayer.listeners.ItemDeleteListener
 import com.project100pi.pivideoplayer.ui.activity.viewmodel.VideoListViewModel
 
 class VideoListViewModelFactory(private val context: Context,
                                 private val folderPath: String,
-                                private val application: Application
-) : ViewModelProvider.Factory {
+                                private val folderName: String,
+                                private val itemDeleteListener: ItemDeleteListener) : ViewModelProvider.Factory {
+
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+
         if (modelClass.isAssignableFrom(VideoListViewModel::class.java)) {
-            return VideoListViewModel(
-                context,
-                folderPath,
-                application
-            ) as T
+
+            return VideoListViewModel(context, folderPath, folderName, itemDeleteListener) as T
+
         }
         throw IllegalArgumentException("Unknown ViewModel class")
     }
