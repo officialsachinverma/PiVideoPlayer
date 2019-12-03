f7d9b33ca1275ff49609a4d8efb2b67c0a524543
Sachin Verma <dev.sachinverma@hotmail.com>
2019-12-03 18:31:01 +0530

    Type : MISC Title : NA ID : NA Description : Added comments and changed method names What was changed : msPlay -> onPlay, msPause -> onPause, msSkipToNext -> onSkipToNext, msSkipToPrevious -> onSkipToPrevious Why was it changed : To improve readability

--- library/src/main/java/com/project100pi/library/listeners/MediaSessionListener.kt
+++ library/src/main/java/com/project100pi/library/listeners/MediaSessionListener.kt
@@ -1,8 +1,40 @@
 package com.project100pi.library.listeners
 
+import android.support.v4.media.session.MediaSessionCompat
+
+/**
+ * This interface provides two methods
+ * The caller of this interface methods
+ * should implement [MediaSessionCompat.Callback]
+ * The implementer of of this interface will get
+ * event requests based on media button clicks.
+ *
+ * @since v1
+ *
+ * [onPlay]
+ * [onPause]
+ * [onSkipToNext]
+ * [onSkipToPrevious]
+ */
 interface MediaSessionListener{
-    fun msPlay()
-    fun msPause()
-    fun msSkipToNext()
-    fun msSkipToPrevious()
+
+    /**
+     * Called when requests to begin playback.
+     */
+    fun onPlay()
+
+    /**
+     * Called when requests to pause playback.
+     */
+    fun onPause()
+
+    /**
+     * Called when requests to skip to the next media item.
+     */
+    fun onSkipToNext()
+
+    /**
+     * Called when requests to skip to the previous media item.
+     */
+    fun onSkipToPrevious()
 }
