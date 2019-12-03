ebf78f62fe73175b90dde2b416b7429198e86e87
Sachin Verma <dev.sachinverma@hotmail.com>
2019-12-03 10:53:38 +0530

    Type : MISC Title : NA ID : NA Description : Added SearchResultAdapter and SearchResultViewHolder What was changed : Added SearchResultAdapter and SearchResultViewHolder Why was it changed : Earlier search result activity was using VideoFilesAdapter which was using VideoListActivity.mIsMultiSelectMode variable. this variable was set in VideoListActivity, providing false values in SearchActivity. So we separated out the adapter and view holder for search results.

---
+++ app/src/main/java/com/project100pi/pivideoplayer/ui/adapters/SearchResultAdapter.kt
@@ -0,0 +1,120 @@
+package com.project100pi.pivideoplayer.ui.adapters
+
+import android.content.Context
+import android.util.SparseBooleanArray
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import com.project100pi.pivideoplayer.listeners.OnClickListener
+import com.project100pi.pivideoplayer.model.VideoTrackInfo
+import com.project100pi.pivideoplayer.ui.activity.SearchActivity
+import com.project100pi.pivideoplayer.ui.adapters.viewholder.SearchResultViewHolder
+
+/**
+ * Created by Sachin Verma on 2019-11-28.
+ */
+
+class SearchResultAdapter(
+    private val context: Context,
+    var view: Int,
+    private var listener: OnClickListener): ListAdapter<VideoTrackInfo, SearchResultViewHolder>(PlayerDiffUtil()) {
+
+    private var selectedItems = SparseBooleanArray()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int)
+            = SearchResultViewHolder(
+                context,
+                LayoutInflater.from(parent.context).inflate(view, parent, false),
+                listener,
+                this)
+
+    override fun onBindViewHolder(holder: SearchResultViewHolder, position: Int) {
+        holder.bind(getItem(position), position)
+    }
+
+    class PlayerDiffUtil: DiffUtil.ItemCallback<VideoTrackInfo>() {
+
+        override fun areItemsTheSame(oldItem: VideoTrackInfo, newItem: VideoTrackInfo): Boolean {
+            return oldItem._Id == newItem._Id
+        }
+
+        override fun areContentsTheSame(oldItem: VideoTrackInfo, newItem: VideoTrackInfo): Boolean {
+            return oldItem == newItem
+        }
+
+    }
+
+    /**
+     * Tells if item on position is selected or not
+     *
+     * @param position Int Position of item
+     * @return Boolean true if selected, otherwise false
+     */
+    fun isSelected(position: Int): Boolean {
+        return getSelectedItems().contains(position)
+    }
+
+    /**
+     * Toggles Selection
+     *
+     * @param position Int Position of item
+     */
+    fun toggleSelection(position: Int) {
+
+        if (selectedItems.get(position, false)) {
+            selectedItems.delete(position)
+        } else {
+            selectedItems.put(position, true)
+        }
+        notifyItemChanged(position)
+    }
+
+    /**
+     * Clears the selection
+     */
+    fun clearSelection() {
+        val selection = getSelectedItems()
+        selectedItems.clear()
+        for (i in selection) {
+            notifyItemChanged(i)
+        }
+    }
+
+    /**
+     * Returns the count of selected items
+     *
+     * @return Int
+     */
+    fun getSelectedItemCount(): Int {
+        return selectedItems.size()
+    }
+
+    /**
+     * Returns a list indices of the selected items
+     *
+     * @return List<Int>
+     */
+    fun getSelectedItems(): List<Int> {
+        val items = ArrayList<Int>(selectedItems.size())
+        for (i in 0 until selectedItems.size()) {
+            items.add(selectedItems.keyAt(i))
+        }
+        return items
+    }
+
+    /**
+     * Toggles all item's selection true
+     */
+    fun selectAllItems() {
+        if (SearchActivity.mIsMultiSelectMode) {
+            this.clearSelection()
+            var i = 0
+            val size: Int = itemCount
+            while (i < size) {
+                listener.onItemSelected(i)
+                i++
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/project100pi/pivideoplayer/ui/adapters/viewholder/SearchResultViewHolder.kt
@@ -0,0 +1,94 @@
+package com.project100pi.pivideoplayer.ui.adapters.viewholder
+
+import android.content.Context
+import android.net.Uri
+import android.view.View
+import android.widget.ImageView
+import android.widget.PopupMenu
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.project100pi.pivideoplayer.R
+import com.project100pi.pivideoplayer.listeners.OnClickListener
+import com.project100pi.pivideoplayer.model.VideoTrackInfo
+import com.project100pi.pivideoplayer.ui.activity.SearchActivity
+import com.project100pi.pivideoplayer.ui.adapters.SearchResultAdapter
+import com.project100pi.pivideoplayer.utils.UtilFunctions
+import java.io.File
+
+/**
+ * Created by Sachin Verma on 2019-11-28.
+ */
+
+class SearchResultViewHolder(private val context: Context, itemView: View, private val listener: OnClickListener, private val adapter: SearchResultAdapter):
+    RecyclerView.ViewHolder(itemView),
+    View.OnClickListener,
+    View.OnLongClickListener {
+
+    private var clItemRow: ConstraintLayout = itemView.findViewById(R.id.cl_video_item_row)
+    private var tvTitle: TextView = itemView.findViewById(R.id.tv_video_name)
+    private var tvDuration: TextView = itemView.findViewById(R.id.tv_video_duration)
+    private var ivThumbnail: ImageView = itemView.findViewById(R.id.iv_video_thumnail)
+    private var ivOverFlow: ImageView = itemView.findViewById(R.id.iv_video_overflow_menu)
+
+    /**
+     * Sets data on views
+     *
+     * @param videoTrack VideoTrackInfo contains information of video
+     * @param position Int Position of the item
+     */
+    fun bind(videoTrack: VideoTrackInfo, position: Int) {
+
+        if(adapter.isSelected(position)){
+            clItemRow.setBackgroundColor(context.resources.getColor(android.R.color.holo_green_dark))
+        }
+        else{
+            clItemRow.setBackgroundColor(context.resources.getColor(android.R.color.white))
+        }
+
+        Glide
+            .with(context)
+            .asBitmap()
+            .load(Uri.fromFile(File(videoTrack.videoPath)))
+            .thumbnail(0.1f)
+            .into(ivThumbnail)
+
+        ivOverFlow.visibility = View.VISIBLE
+        tvDuration.visibility = View.VISIBLE
+        tvDuration.text = UtilFunctions.convertSecondsToHMmSs(videoTrack.durationInMs)
+        tvTitle.text = videoTrack.videoName
+
+        // Setting Listeners
+
+        clItemRow.setOnClickListener(this)
+        clItemRow.setOnLongClickListener(this)
+        ivOverFlow.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View?) {
+        when (view!!.id) {
+            R.id.cl_video_item_row -> listener.onItemSelected(adapterPosition)
+            R.id.iv_video_overflow_menu -> if (!SearchActivity.mIsMultiSelectMode) overflowItemClicked(view, adapterPosition)
+        }
+    }
+
+    override fun onLongClick(p0: View?) = listener.onItemLongClicked(adapterPosition)
+
+    private fun overflowItemClicked(view: View, position: Int) {
+        if (!SearchActivity.mIsMultiSelectMode) {
+
+            val popupMenu = PopupMenu(context, view)
+
+            popupMenu.inflate(R.menu.item_overflow_menu)
+
+            popupMenu.setOnMenuItemClickListener { item ->
+                listener.onOverflowItemClick(position, item.itemId)
+                true
+            }
+
+            popupMenu.show()
+        }
+    }
+
+}
