7f7e7ca834afb5bd4efe0d48444a23682466a82e
Sachin Verma <dev.sachinverma@hotmail.com>
2019-12-03 10:33:52 +0530

    Type : MISC Title : NA ID : NA Description : Added constants What was changed : Added new constants and separated out already existing ones Why was it changed :

--- app/src/main/java/com/project100pi/pivideoplayer/utils/Constants.kt
+++ app/src/main/java/com/project100pi/pivideoplayer/utils/Constants.kt
@@ -4,14 +4,24 @@ object Constants {
 
     object Playback {
         const val WINDOW = "Window"
+        const val PLAYBACK_POSITION = "playbackPosition"
+        const val CURRENT_POSITION = "currentWindow"
+        const val VIDEO_LIST = "videoList"
+        const val PLAYBACK_QUEUE = "playBackQueue"
     }
-    const val PERMISSION_REQUEST_CODE = 100
-    const val FILE_PATH = "AbsolutePath"
-    const val QUEUE = "queue"
-    const val SONG_VIEW = 1
-    const val FOLDER_VIEW = 2
-    const val SHARED_PREFERENCES = "SharedPreferences"
-    const val SD_CARD_URI = "sdCardUri"
+
+    object Permission {
+        const val WRITE_PERMISSION_REQUEST_CODE = 100
+        const val SD_CARD_WRITE_PERMISSION_REQUEST_CODE = 200
+    }
+
+    object TinyDB {
+        const val SHARED_PREFERENCES = "SharedPreferences"
+    }
+
+    object ExternalSDCard {
+        const val SD_CARD_URI = "sdCardUri"
+    }
 
     object Storage {
         const val DIRECTORY_NAME = "directoryName"
@@ -20,5 +30,16 @@ object Constants {
 
     object SearchSource {
         const val SEARCH_LOCAL = "searchLocal"
+    }
+
+    object Orientation {
+        const val PORTRAIT = "portrait"
+        const val LANDSCAPE = "landscape"
+    }
+
+    object Delete {
+        const val SUCCESS = 0
+        const val FAILURE = 1
+        const val SD_CARD_PERMISSION_REQUIRED = 2
     }
 }
