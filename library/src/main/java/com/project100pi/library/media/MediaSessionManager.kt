efdfc33217b14453fe5b558ab650afeefdba44c9
Sachin Verma <dev.sachinverma@hotmail.com>
2019-12-03 18:23:50 +0530

    Type : MISC Title : NA ID : NA Description : 1. Added comments 2. Changed media session listener method names 3. changed secondary constructor to primary constructor What was changed : 1. Added comments 2. msPause -> onPause, msPlay -> onPlay, msSkipToPrevious -> onSkipToPrevious, msSkipToNext -> onSkipToNext 3. Moved params of secondary constructor to primary constructor Why was it changed : 1. To improve readability 2. It was confusing that media transport controls are also using same name for the callbacks so we changed the media session callback name 3. Use of secondary constructor wasnt really required and things can be done with primary constructor and init block

--- library/src/main/java/com/project100pi/library/media/MediaSessionManager.kt
+++ library/src/main/java/com/project100pi/library/media/MediaSessionManager.kt
@@ -4,22 +4,17 @@ import android.app.PendingIntent
 import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
-import android.media.AudioManager
 import android.support.v4.media.session.MediaControllerCompat
 import android.support.v4.media.session.MediaSessionCompat
 import com.project100pi.library.misc.ApplicationHelper
 import com.project100pi.library.misc.Logger
 import com.project100pi.library.listeners.MediaSessionListener
 
-class MediaSessionManager: MediaSessionCompat.Callback {
+class MediaSessionManager(private val appContext: Context, private val mediaSessionListener: MediaSessionListener) : MediaSessionCompat.Callback() {
 
-    private val TAG = "MediaSessionManager"
-
-    private val appContext: Context
     private val mediaSessionCompat: MediaSessionCompat
-    private val mediaSessionListener: MediaSessionListener
 
-    constructor(appContext: Context, mediaSessionListener: MediaSessionListener) {
+    init {
         /*
          * Media Session and Media Session Connector automatically
          * handles a basic set of playback actions such as play, pause,
@@ -27,64 +22,86 @@ class MediaSessionManager: MediaSessionCompat.Callback {
          * For these basic actions putting listeners is not required.
          */
 
-        this.appContext = appContext
-        this.mediaSessionListener = mediaSessionListener
-
         val mediaButtonReceiver = ComponentName(appContext, MediaReceiver::class.java)
+
         val mediaButtonIntent = Intent(Intent.ACTION_MEDIA_BUTTON)
+
         mediaButtonIntent.`package` = appContext.packageName
+
         mediaButtonIntent.setClass(appContext, MediaReceiver::class.java)
 
         val pendingIntent = PendingIntent.getBroadcast(appContext, 0, mediaButtonIntent, 0)
 
         mediaSessionCompat = MediaSessionCompat(appContext, "MediaSessionManager", mediaButtonReceiver, pendingIntent)
-        mediaSessionCompat.setFlags(
-            MediaSessionCompat.FLAG_HANDLES_MEDIA_BUTTONS or MediaSessionCompat.FLAG_HANDLES_TRANSPORT_CONTROLS
-        )
-        mediaSessionCompat.setCallback(this)
-        mediaSessionCompat.isActive = true
-        storeTokenAndID()
 
-//        val mAudioManager =  appContext.getSystemService(Context.AUDIO_SERVICE) as AudioManager
-//        mAudioManager.registerMediaButtonEventReceiver(mediaButtonReceiver)
+        mediaSessionCompat.setFlags(MediaSessionCompat.FLAG_HANDLES_MEDIA_BUTTONS or MediaSessionCompat.FLAG_HANDLES_TRANSPORT_CONTROLS)
+        // register media session callback
+        mediaSessionCompat.setCallback(this)
+        // On making the media session active android system knows that latest active media session
+        // and hence system will provide all the callbacks and all to the latest media session
+        mediaSessionCompat.isActive = true
+        // Soft saving the session token (the saved token will remain live till the app is alive)
+        storeTokenAndID()
     }
 
+    /**
+     * Gets a TransportControls instance for this session.
+     *
+     * @return A controls instance
+     */
     fun getTransportControls(): MediaControllerCompat.TransportControls = mediaSessionCompat.controller.transportControls
 
+    /**
+     * Retrieves a token object that can be used for interacting with this session.
+     * The owner of the session is responsible for deciding how to distribute these
+     * tokens.
+     *
+     * On platform versions before
+     * {@link android.os.Build.VERSION_CODES#LOLLIPOP} this token may only be
+     * used within your app as there is no way to guarantee other apps are using
+     * the same version of the support library.
+     *
+     * @return A token that can be used to create a media controller for this
+     *         session.
+     */
     fun getMediaSessionToken(): MediaSessionCompat.Token = mediaSessionCompat.sessionToken
 
     override fun onMediaButtonEvent(mediaButtonEvent: Intent): Boolean {
         Logger.i("onMediaButtonEvent() called with: mediaButtonEvent = [" + mediaButtonEvent.action + "]")
+
         MediaReceiver.handleMediaButtonIntent(appContext, mediaButtonEvent)
+
         return true
     }
 
     override fun onPause() {
         //player?.playWhenReady = false
-        mediaSessionListener.msPause()
+        mediaSessionListener.onPause()
     }
 
     override fun onPlay() {
         //player?.playWhenReady = true
-        mediaSessionListener.msPlay()
+        mediaSessionListener.onPlay()
     }
 
     override fun onSkipToPrevious() {
         //player?.previous()
-        mediaSessionListener.msSkipToPrevious()
+        mediaSessionListener.onSkipToPrevious()
     }
 
     override fun onSkipToNext() {
         //player?.next()
-        mediaSessionListener.msSkipToNext()
+        mediaSessionListener.onSkipToNext()
     }
 
+    /**
+     * Stores media session token in application Helper class
+     */
     private fun storeTokenAndID() {
 
         Logger.i("storeTokenAndID() :: storing Token and ID for further use")
 
         ApplicationHelper.mediaSessionToken = getMediaSessionToken()
-        //ApplicationHelper.setMediaPlayerAudioSessionId(playbackManager.getMediaPlayerAudioSessionId())
 
         Logger.i("storeTokenAndID() :: successfully stored Token and ID")
     }
