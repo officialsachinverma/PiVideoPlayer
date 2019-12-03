58d41fdc8c0cabb816487b47b87691b15f66251d
Sachin Verma <dev.sachinverma@hotmail.com>
2019-12-03 11:07:22 +0530

    Type : MISC Title : NA ID : NA Description : Name changed for class, added comments and indentation What was changed : 1. Name changed StorageFileAdapter -> FoldersAdapter 2. Added comments 3. Corrected indentation for FoldersViewHolder params Why was it changed :

--- app/src/main/java/com/project100pi/pivideoplayer/ui/adapters/FoldersAdapter.kt
+++ app/src/main/java/com/project100pi/pivideoplayer/ui/adapters/FoldersAdapter.kt
@@ -8,28 +8,31 @@ import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import com.project100pi.pivideoplayer.ui.activity.DirectoryListActivity
 import com.project100pi.pivideoplayer.listeners.OnClickListener
-import com.project100pi.pivideoplayer.ui.adapters.viewholder.StorageFileViewHolder
+import com.project100pi.pivideoplayer.ui.adapters.viewholder.FoldersViewHolder
 import com.project100pi.pivideoplayer.model.FolderInfo
 
-class StorageFileAdapter(
+class FoldersAdapter(
     private val context: Context,
     var view: Int,
-    var listener: OnClickListener): ListAdapter<FolderInfo, StorageFileViewHolder>(PlayerDiffUtil()) {
+    var listener: OnClickListener): ListAdapter<FolderInfo, FoldersViewHolder>(PlayerDiffUtil()) {
 
     private var selectedItems = SparseBooleanArray()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int)
-            = StorageFileViewHolder(context,
-        LayoutInflater.from(parent.context)
-            .inflate(view, parent, false), listener, this)
+            = FoldersViewHolder(
+                context,
+                LayoutInflater.from(parent.context).inflate(view, parent, false),
+                listener,
+        this)
 
-    override fun onBindViewHolder(holder: StorageFileViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: FoldersViewHolder, position: Int) {
         holder.bind(getItem(position), position)
     }
 
     class PlayerDiffUtil: DiffUtil.ItemCallback<FolderInfo>() {
+
         override fun areItemsTheSame(oldItem: FolderInfo, newItem: FolderInfo): Boolean {
-            return (oldItem.folderPath == newItem.folderPath) && (oldItem.filesList.size == newItem.filesList.size)
+            return (oldItem.folderPath == newItem.folderPath) && (oldItem.videoInfoList.size == newItem.videoInfoList.size)
         }
 
         override fun areContentsTheSame(oldItem: FolderInfo, newItem: FolderInfo): Boolean {
@@ -37,10 +40,21 @@ class StorageFileAdapter(
         }
     }
 
+    /**
+     * Tells if item on position is selected or not
+     *
+     * @param position Int Position of item
+     * @return Boolean true if selected, otherwise false
+     */
     fun isSelected(position: Int): Boolean {
         return getSelectedItems().contains(position)
     }
 
+    /**
+     * Toggles Selection
+     *
+     * @param position Int Position of item
+     */
     fun toggleSelection(position: Int) {
 
         if (selectedItems.get(position, false)) {
@@ -51,6 +65,9 @@ class StorageFileAdapter(
         notifyItemChanged(position)
     }
 
+    /**
+     * Clears the selection
+     */
     fun clearSelection() {
         val selection = getSelectedItems()
         selectedItems.clear()
@@ -59,10 +76,20 @@ class StorageFileAdapter(
         }
     }
 
+    /**
+     * Returns the count of selected items
+     *
+     * @return Int
+     */
     fun getSelectedItemCount(): Int {
         return selectedItems.size()
     }
 
+    /**
+     * Returns a list indices of the selected items
+     *
+     * @return List<Int>
+     */
     fun getSelectedItems(): List<Int> {
         val items = ArrayList<Int>(selectedItems.size())
         for (i in 0 until selectedItems.size()) {
@@ -71,20 +98,26 @@ class StorageFileAdapter(
         return items
     }
 
+    /**
+     * Toggles all item's selection true
+     */
     fun selectAllItems() {
         if (DirectoryListActivity.mIsMultiSelectMode) {
             this.clearSelection()
             var i = 0
             val size: Int = itemCount
             while (i < size) {
-                listener.onDirectorySelected(i)
+                listener.onItemSelected(i)
                 i++
             }
         }
     }
 
+    /**
+     * Return the item for the given position
+     *
+     * @param position Int Position of item
+     * @return FolderInfo Object contains folder information
+     */
     fun getInternalItem(position: Int): FolderInfo = getItem(position)
-
-
-
 }
