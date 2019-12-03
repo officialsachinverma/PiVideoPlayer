fc49b4e1e7353d3aad21747daec74f29f1bc493c
Sachin Verma <dev.sachinverma@hotmail.com>
2019-12-03 18:43:56 +0530

    Type : MISC Title : NA ID : NA Description : Corrected indentation What was changed : Why was it changed :

--- library/src/main/java/com/project100pi/library/adapter/CurrentPlayingQueueAdapter.kt
+++ library/src/main/java/com/project100pi/library/adapter/CurrentPlayingQueueAdapter.kt
@@ -2,10 +2,7 @@ package com.project100pi.library.adapter
 
 import android.content.Context
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import androidx.recyclerview.widget.DiffUtil
-import androidx.recyclerview.widget.ListAdapter
 import com.project100pi.library.adapter.viewholder.CurrentPlayingQueueViewHolder
 import com.project100pi.library.dialogs.listeners.OnItemClickListener
 import com.project100pi.library.model.VideoMetaData
@@ -18,20 +15,21 @@ import kotlin.collections.ArrayList
 
 
 class CurrentPlayingQueueAdapter(
-    private val context: Context,
-    var view: Int,
-    private val currentPlayingList: ArrayList<VideoMetaData>,
-    private val nowPlaying: VideoMetaData,
-    var listener: OnItemClickListener
-): RecyclerView.Adapter<CurrentPlayingQueueViewHolder>(), ItemTouchHelperAdapter {
+            private val context: Context,
+            var view: Int,
+            private val currentPlayingList: ArrayList<VideoMetaData>,
+            private val nowPlaying: VideoMetaData,
+            var listener: OnItemClickListener): RecyclerView.Adapter<CurrentPlayingQueueViewHolder>(), ItemTouchHelperAdapter {
 
     var nowPlayingViewHolder: CurrentPlayingQueueViewHolder? = null
     private var mDragStartListener: OnStartDragListener? = null
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int)
-            = CurrentPlayingQueueViewHolder(context,
-        LayoutInflater.from(parent.context)
-            .inflate(view, parent, false), listener, this)
+            = CurrentPlayingQueueViewHolder(
+                context,
+                LayoutInflater.from(parent.context).inflate(view, parent, false),
+                listener,
+        this)
 
     override fun onBindViewHolder(holder: CurrentPlayingQueueViewHolder, position: Int) {
         if (currentPlayingList[position]._id == nowPlaying._id)
@@ -59,17 +57,6 @@ class CurrentPlayingQueueAdapter(
     override fun onItemDismiss(position: Int) {
         currentPlayingList.removeAt(position)
         notifyItemRemoved(position)
-    }
-
-    class PlayerDiffUtil: DiffUtil.ItemCallback<VideoMetaData>() {
-        override fun areItemsTheSame(oldItem: VideoMetaData, newItem: VideoMetaData): Boolean {
-            return oldItem._id == newItem._id
-        }
-
-        override fun areContentsTheSame(oldItem: VideoMetaData, newItem: VideoMetaData): Boolean {
-            return oldItem == newItem
-        }
-
     }
 
 }
