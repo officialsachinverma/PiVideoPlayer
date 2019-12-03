5a40687b3905baafd1416e07fe51e77459c0de37
Sachin Verma <dev.sachinverma@hotmail.com>
2019-12-03 10:46:33 +0530

    Type : MISC Title : NA ID : NA Description : Corrected indentation and added comments What was changed : 1. Last param of primary constructor and extended class 2. Corrected VideoFilesViewHolder param indentation Why was it changed :

--- app/src/main/java/com/project100pi/pivideoplayer/ui/adapters/VideoFilesAdapter.kt
+++ app/src/main/java/com/project100pi/pivideoplayer/ui/adapters/VideoFilesAdapter.kt
@@ -14,21 +14,23 @@ import com.project100pi.pivideoplayer.model.VideoTrackInfo
 class VideoFilesAdapter(
     private val context: Context,
     var view: Int,
-    private var listener: OnClickListener
-): ListAdapter<VideoTrackInfo, VideoFilesViewHolder>(PlayerDiffUtil()) {
+    private var listener: OnClickListener): ListAdapter<VideoTrackInfo, VideoFilesViewHolder>(PlayerDiffUtil()) {
 
     private var selectedItems = SparseBooleanArray()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int)
-            = VideoFilesViewHolder(context,
-        LayoutInflater.from(parent.context)
-            .inflate(view, parent, false), listener, this)
+            = VideoFilesViewHolder(
+                context,
+                LayoutInflater.from(parent.context).inflate(view, parent, false),
+                listener,
+        this)
 
     override fun onBindViewHolder(holder: VideoFilesViewHolder, position: Int) {
         holder.bind(getItem(position), position)
     }
 
     class PlayerDiffUtil: DiffUtil.ItemCallback<VideoTrackInfo>() {
+
         override fun areItemsTheSame(oldItem: VideoTrackInfo, newItem: VideoTrackInfo): Boolean {
             return oldItem._Id == newItem._Id
         }
@@ -39,10 +41,21 @@ class VideoFilesAdapter(
 
     }
 
+    /**
+     * Tells if item on position is selected or not
+     *
+     * @param position Int Position of item
+     * @return Boolean true if selected, otherwise false
+     */
     fun isSelected(position: Int): Boolean {
         return getSelectedItems().contains(position)
     }
 
+    /**
+     * Toggles Selection
+     *
+     * @param position Int Position of item
+     */
     fun toggleSelection(position: Int) {
 
         if (selectedItems.get(position, false)) {
@@ -53,6 +66,9 @@ class VideoFilesAdapter(
         notifyItemChanged(position)
     }
 
+    /**
+     * Clears the selection
+     */
     fun clearSelection() {
         val selection = getSelectedItems()
         selectedItems.clear()
@@ -61,10 +77,20 @@ class VideoFilesAdapter(
         }
     }
 
+    /**
+     * Returns the count of selected items
+     *
+     * @return Int
+     */
     fun getSelectedItemCount(): Int {
         return selectedItems.size()
     }
 
+    /**
+     * Returns a list indices of the selected items
+     *
+     * @return List<Int>
+     */
     fun getSelectedItems(): List<Int> {
         val items = ArrayList<Int>(selectedItems.size())
         for (i in 0 until selectedItems.size()) {
@@ -73,13 +99,16 @@ class VideoFilesAdapter(
         return items
     }
 
+    /**
+     * Toggles all item's selection true
+     */
     fun selectAllItems() {
         if (VideoListActivity.mIsMultiSelectMode) {
             this.clearSelection()
             var i = 0
             val size: Int = itemCount
             while (i < size) {
-                listener.onDirectorySelected(i)
+                listener.onItemSelected(i)
                 i++
             }
         }
